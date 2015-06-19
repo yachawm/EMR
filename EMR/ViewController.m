bcfc433735603ea9bcce80bfb47543c57fad43cc
wenly <yachawm@naver.com>
2015-06-19 17:13:46 +0900

    test3

--- EMR/ViewController.m
+++ EMR/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"commit test");
+    NSLog(@"commit test 2");
 }
 
 - (void)didReceiveMemoryWarning {
